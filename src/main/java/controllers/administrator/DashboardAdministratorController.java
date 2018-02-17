28959f7b0bced3b6b8b6aaca09fd8a8367bcf786
luciadcf <luciadelcarmenfuentes@gmail.com>
2018-02-17 17:53:13 +0100

    Arreglos varios
    - Añadidas querys en AnnouncementRepository, RendezvousRepository
    - Arreglada errata en PopulateDatabase.xml, pero sigue sin terminar

--- src/main/java/controllers/administrator/DashboardAdministratorController.java
+++ src/main/java/controllers/administrator/DashboardAdministratorController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import repositories.RendezvousRepository;
 import controllers.AbstractController;
 
 @Controller
@@ -11,6 +12,7 @@ import controllers.AbstractController;
 public class DashboardAdministratorController extends AbstractController {
 
 	// Services ---------------------------------------------------------------
+	RendezvousRepository rendezvousRepository;
 
 	// Constructors -----------------------------------------------------------
 
@@ -22,11 +24,15 @@ public class DashboardAdministratorController extends AbstractController {
 	@RequestMapping("/display")
 	public ModelAndView display() {
 		ModelAndView result;
+		Double[] avgSqrtRendezvousesPerUser;
 
+		avgSqrtRendezvousesPerUser = this.rendezvousRepository.avgSqrtRendezvousesPerUser();
 		String requestURI = "dashboard/administrator/display.do";
+		
 
 		result = new ModelAndView("dashboard/display");
 
+		result.addObject("avgSqrtRendezvousesPerUser",avgSqrtRendezvousesPerUser);
 		result.addObject("requestURI",requestURI);
 		
 		return result;
--- src/main/java/repositories/RendezvousRepository.java
+++ src/main/java/repositories/RendezvousRepository.java
@@ -1,11 +1,36 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Rendezvous;
 
 @Repository
 public interface RendezvousRepository extends JpaRepository<Rendezvous,Integer> {
+
+	/** Level C **/
+	@Query("")
+	Double[] avgSqrtRendezvousesPerUser();
+	
+	@Query("")
+	Double ratioOfUsersWithRendezvousVsUsersWithoutRendezvous();
+	
+	@Query("")
+	Double[] avgSqrtUsersPerRendezvous();
+	
+	@Query("")
+	Double[] avgSqrtRendezvousesRSVPdPerUser();
+	
+	//The top-10 rendezvouses in terms of users who have RSVPd them. 
+	@Query("")
+	Collection<Rendezvous> top10RendezvousesRSVPd();
+	
+	/** Level B	**/
+	// The rendezvouses that are linked to a number of rendezvouses that is greater than the average plus 10%. 
+	Collection<Rendezvous> redezvousesLinkedPlus10();
+	
 
 }
