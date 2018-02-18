1848978a07f8c1a85e4b2dcea03e20aadfde53ba
marjimveg1 <maria-2696@hotmail.com>
2018-02-18 14:04:17 +0100

    Populate completo

--- src/main/java/domain/Actor.java
+++ src/main/java/domain/Actor.java
@@ -1,3 +1,4 @@
+
 package domain;
 
 import java.util.Date;
@@ -24,87 +25,91 @@ import security.UserAccount;
 public abstract class Actor extends DomainEntity {
 
 	// Constructors -----------------------------------------------------------
-		public Actor() {
-			super();
-		}
-	
+	public Actor() {
+		super();
+	}
+
+
 	// Attributes -------------------------------------------------------------
-		
-		private String name;
-		private String surname;
-		private String postalAddress;
-		private String phoneNumber;
-		private String email;
-		private Date birthDate;
-		
-		@NotBlank
-		public String getName(){
-			return this.name;
-		}
-		
-		public void setName(String name){
-			this.name = name;
-		}
-		
-		@NotBlank
-		public String getSurname(){
-			return this.surname;
-		}
-		
-		public void setSurname(String surname){
-			this.surname = surname;
-		}
-		
-		public String getPostalAddress(){
-			return this.postalAddress;
-		}
-		
-		public void setPostalAddress(String postalAddress){
-			this.postalAddress = postalAddress;
-		}
-		
-		public String getPhoneNumber(){
-			return this.phoneNumber;
-		}
-		
-		public void setPhoneNumber(String phoneNumber){
-			this.phoneNumber = phoneNumber;
-		}
-		
-		@NotBlank
-		@Email
-		public String getEmail(){
-			return this.email;
-		}
-		
-		public void setEmail(String email){
-			this.email = email;
-		}
-		
-		@NotNull
-		@Past
-		@Temporal(TemporalType.DATE)
-		@DateTimeFormat(pattern = "dd/MM/yyyy")
-		public Date getBirthdate() {
-			return birthDate;
-		}
 
-		public void setBirthdate(Date birthDate) {
-			this.birthDate = birthDate;
-		}
-		
-		// Relationships --------------------------------------
-		private UserAccount userAccount;
+	private String	name;
+	private String	surname;
+	private String	postalAddress;
+	private String	phoneNumber;
+	private String	email;
+	private Date	birthDate;
 
-		@NotNull
-		@Valid
-		@OneToOne(cascade = CascadeType.ALL, optional = false)
-		public UserAccount getUserAccount() {
-			return userAccount;
-		}
 
-		public void setUserAccount(UserAccount userAccount) {
-			this.userAccount = userAccount;
-		}
-		
+	@NotBlank
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@NotBlank
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(final String surname) {
+		this.surname = surname;
+	}
+
+	public String getPostalAddress() {
+		return this.postalAddress;
+	}
+
+	public void setPostalAddress(final String postalAddress) {
+		this.postalAddress = postalAddress;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@NotBlank
+	@Email
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	public Date getBirthdate() {
+		return this.birthDate;
+	}
+
+	public void setBirthdate(final Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	// Relationships --------------------------------------
+	private UserAccount	userAccount;
+
+
+	@NotNull
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	public UserAccount getUserAccount() {
+		return this.userAccount;
+	}
+
+	public void setUserAccount(final UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
 }
