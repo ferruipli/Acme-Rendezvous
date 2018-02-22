3479550ff9063344358638eaa31064fc07646720
Alvcalgon <alvcalgon@alum.us.es>
2018-02-22 17:22:07 +0100

    actualizado rendezvousService,userService

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,9 +1,15 @@
 package services;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import domain.Rendezvous;
+import domain.User;
 
 import repositories.UserRepository;
 
@@ -27,7 +33,13 @@ public class UserService {
 	// CRUD methods ---------------------------------------------------------
 	
 	// Other business methods -----------------------------------------------
-	
+	protected void addRendezvous(User user, Rendezvous rendezvous) {
+		Collection<Rendezvous> aux;
+		
+		aux = new HashSet<>(user.getCreatedRendezvouses());
+		aux.add(rendezvous);
+		user.setCreatedRendezvouses(aux);
+	}
 	
 
 }
