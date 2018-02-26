f3d9d612bba898db25a837fa8882805cdbab2d3c
antnolang <antnolang@alum.us.es>
2018-02-26 15:59:45 +0100

    Modificado el modelo de persistencia y el populateDatabase.xml

--- src/main/java/domain/Question.java
+++ src/main/java/domain/Question.java
@@ -6,7 +6,9 @@ import java.util.Collection;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
@@ -41,6 +43,7 @@ public class Question extends DomainEntity {
 
 	// Relationships ----------------------------------------------------
 	private Collection<Answer>	answers;
+	private Rendezvous			rendezvous;
 
 
 	@NotNull
@@ -51,6 +54,17 @@ public class Question extends DomainEntity {
 
 	public void setAnswers(final Collection<Answer> answers) {
 		this.answers = answers;
+	}
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Rendezvous getRendezvous() {
+		return this.rendezvous;
+	}
+
+	public void setRendezvous(final Rendezvous rendezvous) {
+		this.rendezvous = rendezvous;
 	}
 
 }
