511538c42b06adbf189584cf28ae1f307865e1fe
marjimveg1 <maria-2696@hotmail.com>
2018-03-01 22:37:31 +0100

    Corregido cosas Announcement

--- src/main/java/controllers/administrator/AnnouncementAdministratorController.java
+++ src/main/java/controllers/administrator/AnnouncementAdministratorController.java
@@ -3,7 +3,6 @@ package controllers.administrator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,55 +29,25 @@ public class AnnouncementAdministratorController extends AbstractController {
 	}
 
 	// Delete---------------------------------------------
-	@RequestMapping(value = "/display", method = RequestMethod.POST, params = "delete")
-	public ModelAndView delete(final Announcement announcement, final BindingResult binding) {
+
+	@RequestMapping(value = "/remove", method = RequestMethod.GET)
+	public ModelAndView remove(@RequestParam final int announcementId) {
 		ModelAndView result;
+		Announcement announcement;
+
+		result = new ModelAndView("redirect:/rendezvous/list.do");
 
 		try {
+			announcement = this.announcementService.findOne(announcementId);
 			this.announcementService.delete(announcement);
-			result = this.newModelAndView("redirect:../../rendezvous/list.do");
 		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(announcement, "announcement.commit.error");
+			result.addObject("notice", "No se ha podido eliminar el anuncio");
 		}
 
 		return result;
 	}
-
 	// Display--------------------------------------------
-	@RequestMapping(value = "/display", method = RequestMethod.GET)
-	public ModelAndView display(@RequestParam final int announcementId) {
-		ModelAndView result;
-		Announcement announcement;
-
-		announcement = this.announcementService.findOne(announcementId);
-
-		result = new ModelAndView("announcement/display");
-		result.addObject("announcement", announcement);
-
-		return result;
-	}
 
 	// Ancillary methods
-
-	protected ModelAndView createEditModelAndView(final Announcement announcement) {
-		ModelAndView result;
-
-		result = this.createEditModelAndView(announcement, null);
-
-		return result;
-
-	}
-
-	protected ModelAndView createEditModelAndView(final Announcement announcement, final String messageCode) {
-		ModelAndView result;
-
-		result = this.newModelAndView("announcement/create");
-		result.addObject("announcement", announcement);
-
-		result.addObject("message", messageCode);
-
-		return result;
-
-	}
 
 }
