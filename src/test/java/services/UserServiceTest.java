f06279f7f203b8638414660323551ea148932cd8
Alvcalgon <alvcalgon@alum.us.es>
2018-03-19 14:36:04 +0100

    test de req 4.1

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -1,11 +1,7 @@
 package services;
 
 import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
-
-import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,11 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import domain.Comment;
-import domain.RSVP;
-import domain.Rendezvous;
 import domain.User;
 
+import security.UserAccount;
 import utilities.AbstractTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -36,6 +30,11 @@ public class UserServiceTest extends AbstractTest {
 	// Supporting test ------------------------------------------
 	
 	// Test -------------------------------------------------
+	
+	/*
+	 * Requirement 4.1: Register to the system as a user.
+	 * 	Ninguna excepción debería saltar, es un caso de test positivo
+	 */
 	@Test
 	public void testCreate() {
 		User user;
@@ -55,43 +54,137 @@ public class UserServiceTest extends AbstractTest {
 		Assert.isNull(user.getBirthdate());
 	}
 	
+	/*
+	 * Requirement 4.1: Register to the system as a user.
+	 */
+	
 	@Test
 	public void driver() {
 		Object testingData[][] = {
+				// Test positivo
 				{"Name Test 1", "Surname Test 1", "PostalAddress Test 1",
-					"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
-					Collections.<Comment>emptySet(), Collections.<RSVP>emptySet(),
-					Collections.<Rendezvous>emptySet(), null},
-				{"", "Surname Test 2", "PostalAddress Test 2",
-					"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
-					Collections.<Comment>emptySet(), Collections.<RSVP>emptySet(),
-					Collections.<Rendezvous>emptySet(), ConstraintViolationException.class},
-				{"Name Test 3", "Surname Test 3", "PostalAddress Test 3",
 						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
-						Collections.<Comment>emptySet(), Collections.<RSVP>emptySet(),
-						Collections.<Rendezvous>emptySet(), ConstraintViolationException.class},
+						"usernameTest1", "passwordTest1", null},
+				// User::userAccount::password size=5
+				{"Name Test 2", "Surname Test 2", "PostalAddress Test 2",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), 
+						"usernameTest2", "pass5", null},
+				// User::userAccount::password size=6
+				{"Name Test 3", "Surname Test 3", "PostalAddress Test 3",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), 
+						"usernameTest3", "pass56", null},
+				// User::userAccount::password size=14
+				{"Name Test 4", "Surname Test 4", "PostalAddress Test 4",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), 
+						"usernameTest4", "passwordTest17", null},
+				// User::userAccount::password size=31
+				{"Name Test 5", "Surname Test 5", "PostalAddress Test 5",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), 
+						"usernameTest5", "passwordTest17161415090807YYtT1", null},
+				// User::userAccount::password size=32
+				{"Name Test 6", "Surname Test 6", "PostalAddress Test 6",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), 
+						"usernameTest6", "passwordTest17161415090807YYtT32", null},
+				// User::postalAddress optional attribute
+				{"Name Test 7", "Surname Test 7", "",
+						"+34 955060405", "alvarogoles@gmail.com", this.getDate(2000, 10, 30),
+						"usernameTest7", "passwordTest12", null},
+				// User::phoneNumber optional attribute
+				{"Name Test 8", "Surname Test 8", "Postal Address 8",
+						"", "alvarogoles@gmail.com", this.getDate(1999, 10, 30),
+						"username8", "password13", null},
+				// User::name blank
+				{"", "Surname Test 9", "PostalAddress Test 9",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
+						"usernameTest9", "passwordTest9", javax.validation.ConstraintViolationException.class},
+				// User::name safeHtml
+				{"<script> Hacking Test </script>", "Surname Test 10", "PostalAddress Test 10",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
+						"usernameTest10", "passwordTest10", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::surname blank
+				{"Name Test 11", "", "PostalAddress Test 11",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
+						"usernameTest11", "passwordTest11", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::surname safeHtml
+				{"Name Test 12", "<script> Hacking Test </script>", "PostalAddress Test 12",
+						"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
+						"usernameTest12", "passwordTest12", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::email blank
+				{"Name Test 13", "Surname Test 13", "PostalAddress Test 13",
+						"+34 955060405", "", this.getDate(1997, 10, 30),
+						"usernameTest13", "passwordTest13", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::email safeHtml
+				{"Name Test 14", "Surname Test 14", "PostalAddress Test 14",
+						"+34 955060405", "<script> Hacking Test </script>", this.getDate(1997, 10, 30),
+						"usernameTest14", "passwordTest14", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::phoneNumber pattern
+				{"Name Test 15", "Surname Test 15", "PostalAddress Test 15",
+						"numero de telefono", "alvarogoles@gmail.com", this.getDate(1997, 10, 30),
+						"usernameTest15", "passwordTest15", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::email email
+				{"Name Test 16", "Surname Test 16", "PostalAddress Test 16",
+							"+34 955060405", "correo electronico", this.getDate(1997, 10, 30),
+							"usernameTest16", "passwordTest16", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::birthdate notNull
+				{"Name Test 17", "Surname Test 17", "PostalAddress Test 17",
+							"+34 955060405", "alvarogoles@gmail.com", null,
+							"usernameTest17", "passwordTest17", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::birthdate past
+				{"Name Test 18", "Surname Test 18", "PostalAddress Test 18",
+							"+34 955060405", "alvarogoles@gmail.com", this.getDate(2019, 10, 30),
+							"usernameTest18", "passwordTest18", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::userAccount::password size=4
+				{"Name Test 19", "Surname Test 19", "PostalAddress Test 19",
+							"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30),
+							"usernameTest19", "pass", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::userAccount::password size=33
+				{"Name Test 20", "Surname Test 20", "PostalAddress Test 20",
+							"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), 
+							"usernameTest20", "passwordTest17161415090807YYtT321", org.springframework.dao.DataIntegrityViolationException.class},
+				// User::userAccount::username unique username
+				{"Name Test 21", "Surname Test 21", "PostalAddress Test 21",
+							"+34 955060405", "alvarorex@gmail.com", this.getDate(1997, 10, 30), "user1", "passwordTest21",
+							org.springframework.dao.DataIntegrityViolationException.class}
 		};
+		
+		for(int i=0;i<testingData.length;i++)  {
+			template((String)testingData[i][0],
+					 (String)testingData[i][1],
+					 (String)testingData[i][2],
+					 (String)testingData[i][3],
+					 (String)testingData[i][4],
+					 (Date)testingData[i][5],
+					 (String)testingData[i][6],
+					 (String)testingData[i][7],
+					 (Class<?>)testingData[i][8]);
+		}
+		
 	}
 	
 	protected void template(String name,String surname, String postalAddress,
-			String phoneNumber, String email, Date birthdate,
-			Collection<Comment> comments, Collection<RSVP> reserves,
-			Collection<Rendezvous> createdRendezvouses, Class<?> expected) {
+			String phoneNumber, String email, Date birthdate, String username, String password, Class<?> expected) {
 		Class<?> caught;
 		caught = null;
 		User user;
+		UserAccount userAccount;
 		
 		try {
 			user = this.userService.create();
+			
+			userAccount = user.getUserAccount();
+			userAccount.setUsername(username);
+			userAccount.setPassword(password);
+			
 			user.setName(name);
 			user.setSurname(surname);
 			user.setPostalAddress(postalAddress);
 			user.setPhoneNumber(phoneNumber);
 			user.setEmail(email);
 			user.setBirthdate(birthdate);
-			user.setComments(comments);
-			user.setReserves(reserves);
-			user.setCreatedRendezvouses(createdRendezvouses);
+			user.setUserAccount(userAccount);
+			
+			this.userService.save(user);
+			this.userService.flush();
 		} catch (Throwable oops) {
 			caught = oops.getClass();
 		}
