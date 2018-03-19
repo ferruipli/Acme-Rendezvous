56620e111abd83df73f90148b093cbe58b61396b
luciadcf <luciadelcarmenfuentes@gmail.com>
2018-03-19 17:38:21 +0100

    Avanzados tests

--- src/test/java/services/RendezvousServiceTest.java
+++ src/test/java/services/RendezvousServiceTest.java
@@ -31,12 +31,14 @@ public class RendezvousServiceTest extends AbstractTest {
 	@Autowired
 	private UserService			userService;
 	
+	@Autowired
+	private RSVPService			rsvpService;
+	
 	// Other services ------------------------------------------
 
 	// Test ----------------------------------------------------
 	/**
-	 * Acme Rendezvous:
-	 * Req. 4.2 : An actor who is not authenticated must be able to 
+	 * Req. 1.4.2 : An actor who is not authenticated must be able to 
 	 * list the rendezvouses in the system 
 	 */
 	@Test
@@ -45,7 +47,7 @@ public class RendezvousServiceTest extends AbstractTest {
 	}
 	
 	/**
-	 * Acme Rendezvous:
+	 * Req 1.5.2
 	 * An actor who is authenticated by user must b
 	 * Create a rendezvous, which he's implicitly assumed to attend. 
 	 * Note that a user may edit his or her rendezvouses as long as they aren't saved them in final mode. 
@@ -82,8 +84,8 @@ public class RendezvousServiceTest extends AbstractTest {
 	}
 	
 	/**
-	 * Acme Rendezvous:
-	 * An actor who is authenticated by user must b
+	 * Req 1.5.2
+	 * An actor who is authenticated by user must be able to:
 	 * Create a rendezvous, which he's implicitly assumed to attend. 
 	 * Note that a user may edit his or her rendezvouses as long as they aren't saved them in final mode. 
 	 * Once a rendezvous is saved in final mode, it cannot be edited or deleted by the creator.
@@ -115,7 +117,14 @@ public class RendezvousServiceTest extends AbstractTest {
 		this.rendezvousService.save(rendezvous);
 		
 	}
-	
+	/**
+	 * Req 1.5.2
+	 * An actor who is authenticated by user must be able to:
+	 * Create a rendezvous, which he's implicitly assumed to attend. 
+	 * Note that a user may edit his or her rendezvouses as long as they aren't saved them in final mode. 
+	 * Once a rendezvous is saved in final mode, it cannot be edited or deleted by the creator.
+	 * REMARK: the actor isn't a user
+	 */
 	@Test(expected = ClassCastException.class)
 	public void testManagerCreateRendezvous(){
 		
@@ -141,6 +150,134 @@ public class RendezvousServiceTest extends AbstractTest {
 		rendezvous.setAnnouncements(Collections.<Announcement> emptySet());
 		rendezvous.setQuestions(Collections.<Question> emptySet());
 		this.rendezvousService.save(rendezvous);
+		
+		super.unauthenticate();
+	}
+	
+	/**
+	 * Req 1.5.3
+	 * An actor who is authenticated by user must be able to:
+	 * Update the rendezvouses that he or she's created.
+	 * REMARK: user with own rendezvous
+	 */
+	@Test
+	public void testUpdateOwnRendezvous(){
+		super.authenticate("user2");
+		Rendezvous rendezvous;
+		Calendar c;
+		Date moment;
+		
+		c = Calendar.getInstance();
+		c.set(2018, 04, 25, 20, 00);
+		moment = c.getTime();
+		
+		rendezvous = this.rendezvousService.findOne(super.getEntityId("rendezvous1"));
+		rendezvous.setMoment(moment);
+		rendezvous.setFinalMode(false);
+		this.rendezvousService.save(rendezvous);
+		
+		super.unauthenticate();
+	}
+	
+	
+	/**
+	 * Req 1.5.3
+	 * An actor who is authenticated by user must be able to:
+	 * Update the rendezvouses that he or she's created.
+	 * REMARK: user with not own rendezvous
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testUpdateNotOwnRendezvous(){
+		super.authenticate("user2");
+		Rendezvous rendezvous;
+		Calendar c;
+		Date moment;
+		
+		c = Calendar.getInstance();
+		c.set(2018, 04, 25, 20, 00);
+		moment = c.getTime();
+		
+		rendezvous = this.rendezvousService.findOne(super.getEntityId("rendezvous4"));
+		rendezvous.setMoment(moment);
+		rendezvous.setFinalMode(false);
+		this.rendezvousService.save(rendezvous);
+		
+		super.unauthenticate();
+	}
+	/**
+	 * Req 1.5.3 
+	 * An actor who is authenticated by user must be able to:
+	 * Delete the rendezvouses that he or she's created. 
+	 * Deletion is virtual, that is: the information is not removed from the database, 
+	 * but the rendezvous cannot be updated. 
+	 * Deleted rendezvouses are flagged as such when they are displayed.
+	 * REMARK: user with own rendezvous
+	 */
+	@Test
+	public void testDeleteOwnRendezvous(){
+		super.authenticate("user2");
+		
+		Rendezvous rendezvous;
+		
+		rendezvous = this.rendezvousService.findOne(super.getEntityId("rendezvous1"));
+		this.rendezvousService.remove(rendezvous);
+		
+		super.unauthenticate();
+	}
+	
+	/* TODO: da correcto cuando no debería.. 
+	@Test
+	public void testDeleteNotOwnRendezvous(){
+		super.authenticate("user1");
+		
+		Rendezvous rendezvous;
+		
+		rendezvous = this.rendezvousService.findOne(super.getEntityId("rendezvous4"));
+		this.rendezvousService.remove(rendezvous);
+		
+		super.unauthenticate();
+	}*/
+	
+	/**
+	 * Req 1.5.5
+	 * List the rendezvouses that he or she's RSVPd.
+	 */
+	@Test
+	public void testListRendezvousesRSVPByUser(){
+		super.authenticate("user2");
+		
+		this.rendezvousService.findRendezvousesRSVPByUserId(super.getEntityId("user2"));
+		
+		super.unauthenticate();
+	}
+	
+	//TODO:
+	/**
+	 * Req 1.5.5
+	 * List the rendezvouses that he or she's RSVPd.
+	 * REMARK: try to list rendezvouses RSVPd by other user
+	 */
+	/*@Test
+	public void testListRendezvousesRSVPByOtherUser(){
+		super.authenticate("user2");
+		
+		this.rendezvousService.findRendezvousesRSVPByUserId(super.getEntityId("user3"));
+		
+		super.unauthenticate();
+	}*/
+	
+	/**
+	 * Req 1.6.2
+	 * An actor who is authenticated as an administrator must be able to:
+	 * Remove a rendezvous that he or she thinks is inappropriate.
+	 */
+	@Test
+	public void testRemoveRendezvousInappropriate(){
+		super.authenticate("admin");
+		Rendezvous rendezvous;
+		
+		rendezvous = this.rendezvousService.findOne(super.getEntityId("rendezvous1"));
+		this.rendezvousService.remove(rendezvous);
 		
 		super.unauthenticate();
 	}
