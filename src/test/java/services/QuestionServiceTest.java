de72aa2448f34d6d28cc60a6842d1018246b1f08
Alvcalgon <alvcalgon@alum.us.es>
2018-03-20 17:30:19 +0100

    new test

--- src/test/java/services/QuestionServiceTest.java
+++ src/test/java/services/QuestionServiceTest.java
@@ -33,82 +33,10 @@ public class QuestionServiceTest extends AbstractTest {
 	
 	// Test -----------------------------------------------
 	
-	/*
-	 * Requirement 21.1: Manage the questions that are associated with
-	 *  a rendezvous that he or she is created previously.
-	 */
-	/* Caso de test positivo */
-	@Test
-	public void testCreate() {
-		Question question;
-		int rendezvousId;
-		
-		rendezvousId = super.getEntityId("rendezvous2");
-		question = this.questionService.create(rendezvousId);
-		
-		Assert.notNull(question);
-		Assert.notNull(question.getAnswers());
-		Assert.notNull(question.getRendezvous());
-		Assert.isNull(question.getStatement());
-	}
-	
-	/*
-	 * Requirement 21.1: Manage the questions that are associated with
-	 *  a rendezvous that he or she is created previously.
-	 */
-	
-	/* Caso de test positivo */
-	@Test
-	public void testSave() {
-		super.authenticate("user2");
-		
-		Question question,saved;
-		Collection<Question> all;
-		int rendezvousId;
-		
-		rendezvousId = this.findValidRendezvousID();
-		
-		question = this.questionService.create(rendezvousId);
-		question.setStatement("Statement Test");
-		
-		saved = this.questionService.save(question);
-		
-		all = this.questionService.findAll();
-		
-		Assert.isTrue(all.contains(saved));
-		Assert.isTrue(this.rendezvousService.findOrderedQuestionsByRendezvousId(rendezvousId).contains(saved));
-		
-		super.authenticate(null);
-	}
-	
-	/* Caso de test negativo: Question::statement blank */
-	@Test(expected = ConstraintViolationException.class)
-	public void testInvalidSave1() {
-		super.authenticate("user2");
-		
-		Question question,saved;
-		Collection<Question> all;
-		int rendezvousId;
-		
-		rendezvousId = this.findValidRendezvousID();
-		
-		question = this.questionService.create(rendezvousId);
-		
-		saved = this.questionService.save(question);
-		this.questionService.flush();
-		
-		all = this.questionService.findAll();
-		
-		Assert.isTrue(all.contains(saved));
-		Assert.isTrue(this.rendezvousService.findOrderedQuestionsByRendezvousId(rendezvousId).contains(saved));
-		
-		super.authenticate(null);
-	}
-	
 	/* Caso de test negativo: Question is null */
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidSave2() {
-		super.authenticate("user2");
+		super.authenticate("user3");
 		
 		Question question,saved;
 		Collection<Question> all;
@@ -125,6 +53,75 @@ public class QuestionServiceTest extends AbstractTest {
 		super.authenticate(null);
 	}
 	
+	/* Caso de test positivo en el que se comprueba que una question
+	 * se almacena correctamente. */
+	@Test
+	public void testSave() {
+		super.authenticate("user3");
+		
+		Question question, saved;
+		Collection<Question> all;
+		
+		all = this.questionService.findAll();
+		question = this.findQuestion();
+		
+		Assert.isTrue(all.contains(question));
+		
+		question.setStatement("Edited Statement");
+		saved = this.questionService.save(question);
+		
+		Assert.isTrue(all.contains(saved));
+		
+		super.authenticate(null);
+	}
+	
+	@Test
+	public void driver() {
+		Object testingData[][] = {
+				// Caso de test positivo
+				{"user3", "Statement Test", super.getEntityId("rendezvous4"), null},
+				///* Caso de test negativo: Question::statement blank */
+				{"user3", "", super.getEntityId("rendezvous4"), IllegalArgumentException.class},
+				/* Caso de test negativo: la rendezvous relacionada con la question que se quiere
+					almacenar en la BD tiene Rendezvous::finalMode=true. */
+				{"user2", "Statement 3", super.getEntityId("rendezvous1"), IllegalArgumentException.class},
+				/* Test negativo donde la rendezvous relacionada con la question que se quiere
+					almacenar en la BD tiene Rendezvous::isFlagged=true. */
+				{"user2", "Statement 4", super.getEntityId("rendezvous1"), IllegalArgumentException.class},
+				/*Test negativo donde el creador de la rendezvous relacionada con la question
+					que se quiere almacenar no coincide con el principal. */
+				{"user1", "Statement 5", super.getEntityId("rendezvous4"), IllegalArgumentException.class}
+		};
+		
+		for (int i = 0; i < testingData.length; i++) {
+			template((String)testingData[i][0],
+					 (String)testingData[i][1],
+					 (String)testingData[i][2],
+					 (Class<?>) testingData[i][2]);
+		}
+	}
+	
+	protected void template(String username, String statement, String beanName, Class<?> expected) {
+		Class<?> caught;
+		Question question;
+		int rendezvousId;
+		
+		caught = null;
+		
+		try {
+			super.authenticate(username);
+			rendezvousId = super.getEntityId(beanName);
+			question = this.questionService.create(rendezvousId);
+			question.setStatement(statement);
+			this.questionService.save(question);
+			this.questionService.flush();
+		} catch (Throwable oops) {
+			caught = oops.getClass();
+		}
+		
+		checkExceptions(expected,caught);
+	}
+	
 	/* Devuelve una question de una cita que se puede editar. */
 	private Question findQuestion() {
 		Question result;
@@ -135,23 +132,5 @@ public class QuestionServiceTest extends AbstractTest {
 		
 		return result;
 	}
-	
-	/* Devuelve una cita que se puede editar */
-	private int findValidRendezvousID() {
-		int id;
 		
-		id = super.getEntityId("rendezvous4");
-		
-		return id;
-	}
-	
-	/* Devuelve una cita que no se puede editar */
-	private int findInvalidRendezvousID() {
-		int id;
-		
-		id = super.getEntityId("rendezvous1");
-		
-		return id;
-	}
-	
 }
