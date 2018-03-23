a17c6ad8e52786f1b318995f59ac50fb4d90fd5b
luciadcf <luciadelcarmenfuentes@gmail.com>
2018-03-23 17:06:01 +0100

    Prueba de cookies creditCard

--- src/main/java/controllers/user/RequestUserController.java
+++ src/main/java/controllers/user/RequestUserController.java
@@ -2,11 +2,14 @@ package controllers.user;
 
 import java.util.Collection;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +19,7 @@ import services.RequestService;
 import services.ServicesService;
 import services.UserService;
 import controllers.AbstractController;
+import domain.CreditCard;
 import domain.Rendezvous;
 import domain.Request;
 import domain.Services;
@@ -23,86 +27,111 @@ import domain.User;
 
 @Controller
 @RequestMapping("/request/user")
-public class RequestUserController extends AbstractController{
+public class RequestUserController extends AbstractController {
 
 	// Services ---------------------------------------------------------------
 
 	@Autowired
 	private RequestService requestService;
-	
+
 	@Autowired
 	private ServicesService servicesService;
-	
+
 	@Autowired
-	private UserService		userService;
-		
+	private UserService userService;
+
 	// Constructors -----------------------------------------------------------
-	public RequestUserController(){
+	public RequestUserController() {
 		super();
 	}
-	
+
 	// Creation ---------------------------------------------------------------
 
 	@RequestMapping(value = "/edit", method = RequestMethod.GET)
-	public ModelAndView create(@RequestParam final int serviceId) {
+	public ModelAndView create(@RequestParam final int serviceId,
+			@CookieValue(value = "CreditCard", defaultValue = "0") String cookie) {
 		ModelAndView result;
 		Request request;
 		Services service;
+		CreditCard creditCard;
 
-		service = this.servicesService.findOne(serviceId);
-		request = this.requestService.create();
-		request.setService(service);
-		
-		result = this.createEditModelAndView(request);
+		try {
+			this.userService.findByPrincipal();
+			service = this.servicesService.findOne(serviceId);
+			request = this.requestService.create();
+			request.setService(service);
+
+			if (!cookie.equals("0")) {
+				creditCard = this.requestService.reconstruct(cookie);
+				request.setCreditCard(creditCard);
+			}
+
+			result = this.createEditModelAndView(request);
+		} catch (Throwable oops) {
+			result = new ModelAndView("reditect:../../service/list.do");
+		}
 
 		return result;
 	}
-	
-	// Edition  ---------------------------------------------------------------
-	
+
+	// Edition ---------------------------------------------------------------
+
 	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid Request request, final BindingResult binding) {
+	public ModelAndView save(@Valid Request request,
+			final BindingResult binding, HttpServletResponse response) {
 		ModelAndView result;
 		Rendezvous rendezvous;
-		
+		String cookie;
+
 		rendezvous = request.getRendezvous();
-		
+
 		if (binding.hasErrors())
 			result = this.createEditModelAndView(request);
 		else
 			try {
 				this.requestService.save(request);
-				result = new ModelAndView("redirect:/rendezvous/display.do?rendezvousId="+ String.valueOf(rendezvous.getId()));
+				result = new ModelAndView(
+						"redirect:/rendezvous/display.do?rendezvousId="
+								+ String.valueOf(rendezvous.getId()));
+				cookie = request.getCreditCard().getHolderName() + "."
+						+ request.getCreditCard().getBrandName() + "."
+						+ request.getCreditCard().getNumber() + "."
+						+ request.getCreditCard().getExpirationMonth() + "."
+						+ request.getCreditCard().getExpirationYear() + "."
+						+ request.getCreditCard().getCvvCode();
+				response.addCookie(new Cookie("CreditCard", cookie));
 			} catch (final Throwable oops) {
-				result = this.createEditModelAndView(request, "request.commit.error");
+				result = this.createEditModelAndView(request,
+						"request.commit.error");
 			}
 		return result;
 	}
 
 	// Ancillary methods ------------------------------------------------------
 
-		protected ModelAndView createEditModelAndView(final Request request) {
-			ModelAndView result;
+	protected ModelAndView createEditModelAndView(final Request request) {
+		ModelAndView result;
 
-			result = this.createEditModelAndView(request, null);
+		result = this.createEditModelAndView(request, null);
 
-			return result;
-		}
+		return result;
+	}
 
-		protected ModelAndView createEditModelAndView(final Request request, final String messageCode) {
-			ModelAndView result;
-			User user;
-			Collection<Rendezvous> rendezvouses;
-			
-			user = this.userService.findByPrincipal();
-			rendezvouses = user.getCreatedRendezvouses();
-			
-			result = new ModelAndView("request/edit");
-			result.addObject("request", request);
-			result.addObject("rendezvouses", rendezvouses);
-			result.addObject("message", messageCode);
+	protected ModelAndView createEditModelAndView(final Request request,
+			final String messageCode) {
+		ModelAndView result;
+		User user;
+		Collection<Rendezvous> rendezvouses;
 
-			return result;
-		}
-		
+		user = this.userService.findByPrincipal();
+		rendezvouses = user.getCreatedRendezvouses();
+
+		result = new ModelAndView("request/edit");
+		result.addObject("request", request);
+		result.addObject("rendezvouses", rendezvouses);
+		result.addObject("message", messageCode);
+
+		return result;
+	}
+
 }
--- src/main/java/services/RequestService.java
+++ src/main/java/services/RequestService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import repositories.RequestRepository;
+import domain.CreditCard;
 import domain.Rendezvous;
 import domain.Request;
 
@@ -78,6 +79,26 @@ public class RequestService {
 		Collection<Request> result;
 		
 		result = this.requestRepository.findRequestByRendezvous(rendezvous);
+		
+		return result;
+	}
+	
+	public CreditCard reconstruct(String cookie){
+		String[] creditCard=cookie.split("\\.",6);
+		String holderName = creditCard[0];
+		String brandName = creditCard[1];
+		String number = creditCard[2];
+		String expirationMonth = creditCard[3];
+		String expirationYear = creditCard[4];
+		int cvvCode = Integer.parseInt(creditCard[5]);
+		
+		CreditCard result = new CreditCard();
+		result.setHolderName(holderName);
+		result.setBrandName(brandName);
+		result.setNumber(number);
+		result.setExpirationMonth(expirationMonth);
+		result.setExpirationYear(expirationYear);
+		result.setCvvCode(cvvCode);
 		
 		return result;
 	}
